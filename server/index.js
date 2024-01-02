d40a008ae7f53e33d4b47af85f7d05b8a3894463
Leandro-Pinho <leandroessencia@gmail.com>
2024-01-02 16:59:27 -0300

    configurações iniciais do para colocar o servidor no ar

--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
-import express  from "express";
+import express from "express";
 
-app.use(express());
+const app = express();
+
+
+app.use(express.json());
+
+app.listen(8080, () => {
+    console.log('Servidor funcionando na porta 8080');
+});
