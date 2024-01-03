72a371761dfa36bff99bffe784040e1eba3946d0
Leandro-Pinho <leandroessencia@gmail.com>
2024-01-03 00:21:32 -0300

    criando a função create de livros

--- server/src/controllers/books.js
+++ server/src/controllers/books.js
@@ -10,3 +10,22 @@ export const getBooks = async (req, res) => {
         return res.status(400).json({ error });
     }
 };
+
+export const createBook = async (req, res) => {
+    try {
+        if (req.body.titulo === '' || req.body.num_paginas === '' || req.body.isbn === '' || req.body.editora === '') {
+            return res.status(400).json({ message: 'O campo é obrigatório' });
+        }
+
+        const book = await BookModel.create({
+            titulo: req.body.titulo,
+            num_paginas: req.body.num_paginas,
+            isbn: req.body.isbn,
+            editora: req.body.editora,
+        });
+    
+        return res.status(201).json({ message: "Livro criado com sucesso" });
+    } catch (error) {
+        return res.status(400).json({ error });
+    }
+};
--- server/src/routes/routerBooks.js
+++ server/src/routes/routerBooks.js
@@ -1,11 +1,11 @@
 import express from "express";
 
-import { getBooks } from "../controllers/books.js";
+import { getBooks, createBook } from "../controllers/books.js";
 
 const router = express.Router();
 
 router.get("/books", getBooks)
-// router.post("/user", createUser)
+router.post("/book", createBook)
 // router.get("/user/:id", getUser)
 // router.delete("/user/:id", deleteUser)
 // router.put("/user/:id", updateUser)
